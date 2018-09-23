732d2bb6e81d9d5a396255836c355ef5ce5558b1
Stephen Gordon <gordoste@iinet.net.au>
2018-09-23 21:40:15 +1000

    Rename files and move constructor to begin()

--- src/LedControl_HW_SPI.h
+++ src/LedControl_HW_SPI.h
@@ -24,15 +24,15 @@
  *    OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#ifndef LedControl_h
-#define LedControl_h
+#ifndef LEDCONTROL_HW_SPI_H
+#define LEDCONTROL_HW_SPI_H
 
 //--------------------
 #if (defined(__AVR__))
 #include <avr/pgmspace.h>
 #elif defined(ESP8266)
-#include <pgmspace.h>	// Fix for ESP8266 based board by Simon John 
-#endif			
+#include <pgmspace.h> // Fix for ESP8266 based board by Simon John
+#endif
 //--------------------
 
 #if (ARDUINO >= 100)
@@ -45,72 +45,74 @@
  * Segments to be switched on for characters and digits on
  * 7-Segment Displays
  */
-const static byte charTable [] PROGMEM  = {
-    B01111110,B00110000,B01101101,B01111001,B00110011,B01011011,B01011111,B01110000,    // 000-007 :
-    B01111111,B01111011,B01110111,B00011111,B00001101,B00111101,B01001111,B01000111,    // 008-015 : 
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,    // 016-023 :
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,    // 024-031 :
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,    // 032-039 :
-    B00000000,B00000000,B00000000,B00000000,B10000000,B00000001,B10000000,B00000000,    // 040-047 :
-    B01111110,B00110000,B01101101,B01111001,B00110011,B01011011,B01011111,B01110000,    // 048-055 : 0-7
-    B01111111,B01111011,B00000000,B00000000,B00000000,B00001001,B00000000,B00000000,    // 056-063 : 8-?
-    B00000000,B01110111,B00011111,B01001110,B00111101,B01001111,B01000111,B01011110,    // 064-071 : @-G
-    B00110111,B00110000,B00111100,B00000000,B00001110,B00000000,B00000000,B01111110,    // 072-079 : H-O
-    B01100111,B00000000,B00000000,B01011011,B00000000,B00111110,B00111110,B00000000,    // 080-087 : P-W
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00001000,    // 088-095 : X-_
-    B00000000,B01110111,B00011111,B00001101,B00111101,B01001111,B01000111,B00000000,    // 096-103 : `-g
-    B00110111,B00010000,B00000000,B00000000,B00001110,B00000000,B00010101,B00011101,    // 104-113 : h-q
-    B01100111,B00000000,B00000000,B00000000,B00001111,B00011100,B00000000,B00000000,    // 114-119 : r-w
-    B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000,B00000000     // 120-127 : x-[del]
+const static byte charTable[] PROGMEM = {
+    B01111110, B00110000, B01101101, B01111001, B00110011, B01011011, B01011111, B01110000, // 000-007 :
+    B01111111, B01111011, B01110111, B00011111, B00001101, B00111101, B01001111, B01000111, // 008-015 :
+    B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, // 016-023 :
+    B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, // 024-031 :
+    B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, // 032-039 :
+    B00000000, B00000000, B00000000, B00000000, B10000000, B00000001, B10000000, B00000000, // 040-047 :
+    B01111110, B00110000, B01101101, B01111001, B00110011, B01011011, B01011111, B01110000, // 048-055 : 0-7
+    B01111111, B01111011, B00000000, B00000000, B00000000, B00001001, B00000000, B00000000, // 056-063 : 8-?
+    B00000000, B01110111, B00011111, B01001110, B00111101, B01001111, B01000111, B01011110, // 064-071 : @-G
+    B00110111, B00110000, B00111100, B00000000, B00001110, B00000000, B00000000, B01111110, // 072-079 : H-O
+    B01100111, B00000000, B00000000, B01011011, B00000000, B00111110, B00111110, B00000000, // 080-087 : P-W
+    B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00001000, // 088-095 : X-_
+    B00000000, B01110111, B00011111, B00001101, B00111101, B01001111, B01000111, B00000000, // 096-103 : `-g
+    B00110111, B00010000, B00000000, B00000000, B00001110, B00000000, B00010101, B00011101, // 104-113 : h-q
+    B01100111, B00000000, B00000000, B00000000, B00001111, B00011100, B00000000, B00000000, // 114-119 : r-w
+    B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000, B00000000  // 120-127 : x-[del]
 };
 
-class LedControl {
-    private :
-        /* The array for shifting the data to the devices */
-        byte spidata[16];
-        /* Send out a single command to the device */
-        void spiTransfer(int addr, byte opcode, byte data);
+class LedControl
+{
+private:
+  /* The array for shifting the data to the devices */
+  byte spidata[16];
+  /* Send out a single command to the device */
+  void spiTransfer(int addr, byte opcode, byte data);
 
-        /* We keep track of the led-status for all 8 devices in this array */
-        byte status[64];
-        /* Data is shifted out of this pin*/
-        int SPI_MOSI;
-        /* The clock is signaled on this pin */
-        int SPI_CLK;
-        /* This one is driven LOW for chip selectzion */
-        int SPI_CS;
-        /* The maximum number of devices we use */
-        int maxDevices;
-		/* Max SPI communication speed passed to SPISettings() */
-        unsigned long speedMaximum;
+  /* We keep track of the led-status for all 8 devices in this array */
+  byte status[64];
+  /* Data is shifted out of this pin*/
+  int SPI_MOSI;
+  /* The clock is signaled on this pin */
+  int SPI_CLK;
+  /* This one is driven LOW for chip selectzion */
+  int SPI_CS;
+  /* The maximum number of devices we use */
+  int maxDevices;
+  /* Max SPI communication speed passed to SPISettings() */
+  unsigned long speedMaximum;
 
-    public:
-        /* 
+public:
+  LedControl(){};
+  /* 
          * Create a new controler 
          * Params :
          * csPin		pin for selecting the device 
          * numDevices           maximum number of devices that can be controled
          * spiSpeedMax          Maximum SPI communication speed (passed to SPISettings)
          */
-        LedControl(int csPin, int numDevices=1, unsigned long spiSpeedMax=10000000);
+  void begin(int csPin, int numDevices = 1, unsigned long spiSpeedMax = 10000000);
 
-        /*
+  /*
          * Gets the number of devices attached to this LedControl.
          * Returns :
          * int	the number of devices on this LedControl
          */
-        int getDeviceCount();
+  int getDeviceCount();
 
-        /* 
+  /* 
          * Set the shutdown (power saving) mode for the device
          * Params :
          * addr	The address of the display to control
          * status	If true the device goes into power-down mode. Set to false
          *		for normal operation.
          */
-        void shutdown(int addr, bool status);
+  void shutdown(int addr, bool status);
 
-        /* 
+  /* 
          * Set the number of digits (or rows) to be displayed.
          * See datasheet for sideeffects of the scanlimit on the brightness
          * of the display.
@@ -118,24 +120,24 @@ class LedControl {
          * addr	address of the display to control
          * limit	number of digits to be displayed (1..8)
          */
-        void setScanLimit(int addr, int limit);
+  void setScanLimit(int addr, int limit);
 
-        /* 
+  /* 
          * Set the brightness of the display.
          * Params:
          * addr		the address of the display to control
          * intensity	the brightness of the display. (0..15)
          */
-        void setIntensity(int addr, int intensity);
+  void setIntensity(int addr, int intensity);
 
-        /* 
+  /* 
          * Switch all Leds on the display off. 
          * Params:
          * addr	address of the display to control
          */
-        void clearDisplay(int addr);
+  void clearDisplay(int addr);
 
-        /* 
+  /* 
          * Set the status of a single Led.
          * Params :
          * addr	address of the display 
@@ -144,9 +146,9 @@ class LedControl {
          * state	If true the led is switched on, 
          *		if false it is switched off
          */
-        void setLed(int addr, int row, int col, boolean state);
+  void setLed(int addr, int row, int col, boolean state);
 
-        /* 
+  /* 
          * Set all 8 Led's in a row to a new state
          * Params:
          * addr	address of the display
@@ -154,9 +156,9 @@ class LedControl {
          * value	each bit set to 1 will light up the
          *		corresponding Led.
          */
-        void setRow(int addr, int row, byte value);
+  void setRow(int addr, int row, byte value);
 
-        /* 
+  /* 
          * Set all 8 Led's in a column to a new state
          * Params:
          * addr	address of the display
@@ -164,9 +166,9 @@ class LedControl {
          * value	each bit set to 1 will light up the
          *		corresponding Led.
          */
-        void setColumn(int addr, int col, byte value);
+  void setColumn(int addr, int col, byte value);
 
-        /* 
+  /* 
          * Display a hexadecimal digit on a 7-Segment Display
          * Params:
          * addr	address of the display
@@ -174,9 +176,9 @@ class LedControl {
          * value	the value to be displayed. (0x00..0x0F)
          * dp	sets the decimal point.
          */
-        void setDigit(int addr, int digit, byte value, boolean dp);
+  void setDigit(int addr, int digit, byte value, boolean dp);
 
-        /* 
+  /* 
          * Display a character on a 7-Segment display.
          * There are only a few characters that make sense here :
 		 *	'0','1','2','3','4','5','6','7','8','9','0',
@@ -189,10 +191,7 @@ class LedControl {
          * value	the character to be displayed. 
          * dp	sets the decimal point.
          */
-        void setChar(int addr, int digit, char value, boolean dp);
+  void setChar(int addr, int digit, char value, boolean dp);
 };
 
-#endif	//LedControl.h
-
-
-
+#endif //LEDCONTROL_HW_SPI_H
