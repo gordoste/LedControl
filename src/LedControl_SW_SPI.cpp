1a0af52792d3e0574b15c439cbcf1054dd7b856f
Stephen Gordon <gordoste@iinet.net.au>
2018-09-28 21:58:34 +1000

    Support hardware and software SPI

---
+++ src/LedControl_SW_SPI.cpp
@@ -0,0 +1,46 @@
+#include "LedControl_SW_SPI.h"
+
+void LedControl_SW_SPI::begin(int dataPin, int clkPin, int csPin, int numDevices) {
+    SPI_MOSI=dataPin;
+    SPI_CLK=clkPin;
+    SPI_CS=csPin;
+    if(numDevices<=0 || numDevices>8 )
+        numDevices=8;
+    maxDevices=numDevices;
+    pinMode(SPI_MOSI,OUTPUT);
+    pinMode(SPI_CLK,OUTPUT);
+    pinMode(SPI_CS,OUTPUT);
+    digitalWrite(SPI_CS,HIGH);
+    SPI_MOSI=dataPin;
+    for(int i=0;i<64;i++) 
+        status[i]=0x00;
+    for(int i=0;i<maxDevices;i++) {
+        spiTransfer(i,LEDCTL_OP_DISPLAYTEST,0);
+        //scanlimit is set to max on startup
+        setScanLimit(i,7);
+        //decode is done in source
+        spiTransfer(i,LEDCTL_OP_DECODEMODE,0);
+        clearDisplay(i);
+        //we go into shutdown-mode on startup
+        shutdown(i,true);
+    }
+}
+
+void LedControl_SW_SPI::spiTransfer(int addr, volatile byte opcode, volatile byte data) {
+    //Create an array with the data to shift out
+    int offset=addr*2;
+    int maxbytes=maxDevices*2;
+
+    for(int i=0;i<maxbytes;i++)
+        spidata[i]=(byte)0;
+    //put our device data into the array
+    spidata[offset+1]=opcode;
+    spidata[offset]=data;
+    //enable the line 
+    digitalWrite(SPI_CS,LOW);
+    //Now shift out the data 
+    for(int i=maxbytes;i>0;i--)
+        shiftOut(SPI_MOSI,SPI_CLK,MSBFIRST,spidata[i-1]);
+    //latch the data onto the display
+    digitalWrite(SPI_CS,HIGH);
+}    
---
+++ src/LedControl_SW_SPI.h
@@ -0,0 +1,19 @@
+#ifndef LEDCONTROL_SW_SPI_H
+#define LEDCONTROL_SW_SPI_H
+
+#include "LedControl.h"
+
+class LedControl_SW_SPI : LedControl
+{
+  private:
+    /* Data is shifted out of this pin*/
+    int SPI_MOSI;
+    /* The clock is signaled on this pin */
+    int SPI_CLK;
+    void spiTransfer(int addr, byte opcode, byte data);
+
+  public:
+    void begin(int dataPin, int clkPin, int csPin, int numDevices);
+};
+
+#endif // LEDCONTROL_SW_SPI_H
